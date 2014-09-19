d890cc4cac81528f6a76613ed4fac157ac49f1e6
gnishida <gnishida@purdue.edu>
2014-09-19 01:24:28 -0400

    Initial commit

---
+++ GPGPU.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#define GLEW_STATIC 1
+#include <GL/glew.h>
+#include <GL/glut.h>
+#include <string>
+
+class GPGPU
+{
+public:
+	GPGPU(int w, int h);
+
+	void restart();
+	void update();
+	void display();
+	int loadShader(char* filename, std::string& text);
+
+private:
+    int _width;				// width of the screen
+	int _height;			// height of the screen
+	int _initialized;		// if the cells are initialized (=1) or not (=0)
+
+	GLuint _initializedLoc;
+    GLuint _texUnitLoc;
+    
+    GLuint _textureId;		// The texture ID used to store data array
+    GLuint _programId;		// the program ID
+
+    GLuint _fragmentShader;
+};
+
